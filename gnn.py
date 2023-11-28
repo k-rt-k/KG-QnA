fa25fe6f0628526b97d64d71eb2da50bac3caf88
k-rt-k <94541845+k-rt-k@users.noreply.github.com>
2023-11-28 23:03:19 +0530

    gnn main train

--- gnn.py
+++ gnn.py
@@ -47,3 +47,35 @@ def test(model, data):
     model.eval()
     with torch.no_grad():
         return model.predict(data)
+    
+if __name__ == '__main__':
+    import torch.optim as optim
+    from torch_geometric.data import DataLoader
+    from torch_geometric.nn.kge import ComplEx
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    from generate_graph import graph_to_nx
+
+    G = graph_to_nx("IMDB-Movie-Data.csv")
+
+    data = torch_geometric.utils.from_networkx(G)
+
+    embedding_dim = 64 ## undecided
+
+    num_relations = torch.max(data.type).cpu().item() + 1
+
+    model = ComplEx(data.num_nodes, num_relations, hidden_channels=embedding_dim).to(device)
+
+    print(model)
+
+    loader = model.loader(
+        head_index= data.edge_index[0],
+        rel_type= data.type,
+        tail_index= data.edge_index[1],
+        batch_size=128,
+        shuffle=True,
+    )
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3,weight_decay=1e-6)
+    train(model,optimizer,loader)
+    ### do something with the embeddings
+    
